4db4bbd17c3250f06e8f37f6de172b04f00387c9
Ray <raymond.dee@ontariotechu.net>
2020-02-01 03:37:10 -0500

    [ADD/MOD] HeartMonitorUI Prefab

--- Global Game Jam 2020/Assets/_Scripts/HeartMonitor.cs
+++ Global Game Jam 2020/Assets/_Scripts/HeartMonitor.cs
@@ -4,30 +4,49 @@ using UnityEngine;
 
 public class HeartMonitor : MonoBehaviour
 {
+    private float count = 0.0f;
+    private float amplitude = 0.0f;
     private float tValue = 0.5f;
-    private bool pulse = false;
+    private float period = 0.0f;
+    private Vector3 amplitudeVector;
     [SerializeField]
-    private Transform transform;
+    private Transform localTransform;
     [SerializeField]
+    private Transform emitterTransform;
+    [SerializeField]
+    [Range(0.0f, 100.0f)]
     private float currentHealth;
+
     void Start()
     {
-        StartCoroutine(HeartRate());
+        period = ((Mathf.PI / 6) + 0.1f);
     }
 
     void Update()
     {
-    }
+        amplitudeVector = new Vector3(0.0f, amplitude, 0.0f);
 
-    IEnumerator HeartRate()
-    {
-        for (; ; )
+        tValue = 0.5f * Mathf.Sin(count * 10.0f) + 0.5f;
+
+        emitterTransform.position = Vector3.Lerp(localTransform.position + amplitudeVector, localTransform.position - amplitudeVector, tValue);
+
+        count += Time.deltaTime;
+
+        if (count > Mathf.Max((period + 0.05f), (currentHealth * 0.02f)))
         {
-            tValue = 0.5f * Mathf.Sin(Time.time * 10.0f) + 0.5f;
-
-            transform.position = Vector3.Lerp(new Vector3(0.0f, 5.0f, 0.0f), new Vector3(0.0f, -5.0f, 0.0f), tValue);
-
-            yield return null;
+            StartCoroutine(HeartBeat());
+            count = 0.0f;
         }
     }
+
+    IEnumerator HeartBeat()
+    { 
+
+        amplitude = 5.0f * (currentHealth * 0.01f);
+
+        yield return new WaitForSeconds(period);
+
+        amplitude = 0.0f;
+    }
+
 }
