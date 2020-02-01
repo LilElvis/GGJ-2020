5ec0829fad63c56335f8f82da2e6b3bbefded634
Josh <joshua.sankarlal@ontariotechu.net>
2020-02-01 15:38:21 -0500

    [MOD] Player Model / Movement

--- Global Game Jam 2020/Assets/PlayerMovement.cs
+++ Global Game Jam 2020/Assets/PlayerMovement.cs
@@ -5,27 +5,34 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public Rigidbody rb;
+    public Vector2 movement;
     public float speed = 5;
-    
+    public float friction;
+
     // Start is called before the first frame update
     void Start()
     {
         rb = this.GetComponent<Rigidbody>();
-      
-    }   
+
+    }
 
     // Update is called once per frame
     void Update()
     {
 
-        Vector2 direction = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
-
-
-        rb.AddForce(direction * speed);
+        movement = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        gameObject.transform.Rotate(0, 0, movement.x);
     }
 
+    void moveCharacter(Vector2 direction)
+
+    {
+
+        rb.AddRelativeForce(direction * speed);
+    }
     void FixedUpdate()
     {
+        moveCharacter(movement);
 
     }
 
