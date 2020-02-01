ac2e797d9d2febd2fb8f28528805fff8b27bed63
Josh <joshua.sankarlal@ontariotechu.net>
2020-02-01 00:33:33 -0500

    [ADD] Basic Movement

---
+++ Global Game Jam 2020/Assets/PlayerMovement.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public CharacterController cc;
+    public float speed = 10;
+    public Vector2 movement;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        cc = this.GetComponent<CharacterController>();
+    }   
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        movement.Normalize();
+        cc.Move(movement * Time.deltaTime * speed);
+       
+    }
+
+   
+}
