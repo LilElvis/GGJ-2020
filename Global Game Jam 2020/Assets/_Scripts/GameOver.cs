0c3d7d00513174be4b7b393e43c372827dbc0ff2
Ray <raymond.dee@ontariotechu.net>
2020-02-02 09:46:26 -0500

    [ADD/MOD] Completed rough game loop.

---
+++ Global Game Jam 2020/Assets/_Scripts/GameOver.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class GameOver : MonoBehaviour
+{
+    [SerializeField] private Text gameOverText;
+
+    IEnumerator EndGame()
+    {
+        yield return new WaitForSeconds(5.0f);
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    //Event Listner---------------------------------------------------------------------------------
+    public List<EventRelay.EventMessageType> eventsHandled = new List<EventRelay.EventMessageType>();
+
+    void OnEnable()
+    {
+        EventRelay.OnEventAction += HandleEvent;
+    }
+
+    void OnDisable()
+    {
+        EventRelay.OnEventAction -= HandleEvent;
+    }
+
+    string HandleEvent(EventRelay.EventMessageType messageType, MonoBehaviour sender)
+    {
+        if (messageType == EventRelay.EventMessageType.GameStart)
+        {
+            
+        }
+        if (messageType == EventRelay.EventMessageType.GameOver)
+        {
+            gameOverText.text = "You died!";
+
+            StartCoroutine(EndGame());
+        }
+
+        if (eventsHandled.Contains(messageType))
+        {
+            Debug.Log(this.name + " handled event: " + messageType + " from sender: " + sender);
+            return this.ToString();
+        }
+        else
+        {
+            return this.ToString();
+        }
+    }
+    //------------------------------------------------------------------------------------------------
+}
---
+++ Global Game Jam 2020/Assets/_Scripts/WoundManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WoundManager : MonoBehaviour
+{
+    [SerializeField] private List<GameObject> woundList;
+
+    private float woundBuffer = 15.0f;
+
+    void Start()
+    {
+        InvokeRepeating("ActivateRandomWound", woundBuffer, woundBuffer);
+    }
+
+    void Update()
+    {
+
+    }
+
+    void ActivateRandomWound()
+    {
+        int index = Random.Range(0, woundList.Capacity - 1);
+
+        if (!woundList[index].activeInHierarchy)
+            woundList[index].SetActive(true);
+        else
+            ActivateRandomWound();
+    }
+}
