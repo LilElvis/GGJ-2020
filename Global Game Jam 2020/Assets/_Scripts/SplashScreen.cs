b181eac71ba13148b3936dfe46f1b246c48f9e4f
Ray <raymond.dee@ontariotechu.net>
2020-02-04 04:21:44 -0500

    [MOD] Created new splash screen animation.

--- Global Game Jam 2020/Assets/_Scripts/SplashScreen.cs
+++ Global Game Jam 2020/Assets/_Scripts/SplashScreen.cs
@@ -12,7 +12,7 @@ public class SplashScreen : MonoBehaviour
 
     IEnumerator LoadNextScene()
     {
-        yield return new WaitForSeconds(3.0f);
+        yield return new WaitForSeconds(3.5f);
 
         SceneManager.LoadScene("MainMenu");
     }
